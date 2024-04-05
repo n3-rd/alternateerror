161c07f07b6be25b220926ed83ddd62edd5ed66f
n3-rd <godwinjdayo@gmail.com>
2024-04-05 12:26:46 +0100

    style fixes

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -3,18 +3,35 @@
 	import Card from '../components/Card.svelte';
 	import Welcome from '../components/Welcome.svelte';
 	import type { PageData } from './$types';
+	import { urlFor } from '$lib/sanity/image';
 
 	export let data: PageData;
 	const q = useQuery(data);
 
 	$: ({ data: posts } = $q);
+	console.log($q);
+	console.log(urlFor($q.data[0].mainImage).url());
 </script>
 
-<section>
+<section class="relative">
 	{#if posts.length}
-		{#each posts as post}
-			<Card {post} />
-		{/each}
+		<div class="grid grid-cols-4 gap-7">
+			{#each posts as post}
+				<Card {post} />
+			{/each}
+			{#each posts as post}
+				<Card {post} />
+			{/each}
+			{#each posts as post}
+				<Card {post} />
+			{/each}
+			{#each posts as post}
+				<Card {post} />
+			{/each}
+			{#each posts as post}
+				<Card {post} />
+			{/each}
+		</div>
 	{:else}
 		<Welcome />
 	{/if}
